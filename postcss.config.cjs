5446b8ad109707490364c34a25ab7699ff7cb36e
ToddLadner <tladner03@gmail.com>
2023-10-01 18:22:20 -0500

    PostCss & Styles

---
+++ postcss.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-preset-env')({
+      stage: 0,
+      'nesting-rules': true,
+    }),
+    require('postcss-import'),
+    require('postcss-size'),
+    require('postcss-font-magician'),
+    require('postcss-each'),
+    require('postcss-for'),
+  ],
+}
