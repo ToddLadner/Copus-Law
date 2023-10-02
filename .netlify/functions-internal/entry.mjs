5ffd79b3c3b7fd8fcdd4d1d01b8639f8c639829b
ToddLadner <tladner03@gmail.com>
2023-10-01 21:22:21 -0500

    Build and push

---
+++ .netlify/functions-internal/entry.mjs
@@ -0,0 +1,24 @@
+import * as adapter from '@astrojs/netlify/netlify-functions.js';
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_8d2ac899.mjs';
+import './chunks/astro_66e502e3.mjs';
+import './chunks/pages/image-endpoint_7b651bf1.mjs';
+import './chunks/astro-assets-services_90a31e4d.mjs';
+
+const _page0  = () => import('./chunks/image-endpoint_3e181179.mjs');
+const _page1  = () => import('./chunks/index_4beca039.mjs');const pageMap = new Map([["node_modules/.pnpm/astro@3.2.0/node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/index.astro", _page1]]);
+const _manifest = Object.assign(manifest, {
+	pageMap,
+	renderers,
+});
+const _args = {};
+
+const _exports = adapter.createExports(_manifest, _args);
+const handler = _exports['handler'];
+
+const _start = 'start';
+if(_start in adapter) {
+	adapter[_start](_manifest, _args);
+}
+
+export { handler, pageMap };
