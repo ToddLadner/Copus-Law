56e26528cbcb080182913f30f47beb4b8b3b832d
ToddLadner <tladner03@gmail.com>
2023-10-13 22:24:25 -0500

    404 testimonials swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify/functions'
 
-import netlify from "@astrojs/netlify/functions";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
-});
+  output: 'server',
+  adapter: netlify(),
+  // TODO => site: 'https://stargazers.club',
+  integrations: [sitemap()],
+})
